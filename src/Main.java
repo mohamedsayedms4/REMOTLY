35b526d4acff269f6a7828cd6fefdb64f920f31b
mohamedsayedms4 <mohamedms4002@gmail.com>
2025-06-21 05:18:29 +0300

    build student class with builder

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Student;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        LocalDate dateOfBirth = LocalDate.of(2000, 5, 10);  // 10 مايو 2000
+
+        Student student = Student.StudentBuilder.signUpBuilder(
+                "Ahmed", "Sayed", "ahmedsayed123",
+                "ahmed@gmail.com", "1223456", "01007809006",
+                "male", dateOfBirth
+        ).build();
+
+        Student student1 = Student.StudentBuilder.signUpBuilder(
+                "Ahmed", "Sayed", "ahmedsayed123",
+                "ahmed@gmail.com", "1223456", "01007809006",
+                "male", dateOfBirth
+
+        ).setAddress("Some address").build();
+
+        System.out.println(student);
     }
 }
---
+++ src/model/Role.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum Role {
+    STUDENT,
+    TEACHER,
+    ADMIN
+}
